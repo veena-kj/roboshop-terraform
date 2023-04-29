b2f3a42aadbf1bdd5a78d982709a8df1cf03dcf8
DELL <kolloju.aishwarya@gmail.com>
2023-04-29 16:24:24 +0530

    < variable checks>

--- servers.tf
+++ servers.tf
@@ -1,3 +1,4 @@
+
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.centos_ami.image_id
   instance_type = "t3.micro"
@@ -5,7 +6,12 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
-
+variable "instance_type" {
+  default = "t3.micro"
+}
+variable "zone_id" {
+  default = "Z08406313PSKR2N4EROKD"
+}
 data "aws_ami" "centos_ami"{
   owners = ["973714476881"]
   name_regex = "Centos-8-DevOps-Practice"
@@ -19,9 +25,10 @@ output "frontend" {
 output "frontendIP" {
   value=aws_instance.frontend.private_ip
 }
+
 resource "aws_route53_record" "frontend" {
   zone_id = "Z08406313PSKR2N4EROKD"
-  name    = "frontendd.e.platform.online"
+  name    = "frontendd.e-platform.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.frontend.private_ip]
@@ -29,7 +36,7 @@ resource "aws_route53_record" "frontend" {
 
 resource "aws_instance" "mongodb" {
   ami           = data.aws_ami.centos_ami.image_id
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
   tags = {
     Name = "mongodb"
   }
@@ -37,7 +44,7 @@ resource "aws_instance" "mongodb" {
 
 resource "aws_route53_record" "mongodb" {
   zone_id = "Z08406313PSKR2N4EROKD"
-  name    = "mongod.e.platform.online"
+  name    = "mongod.e-platform.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.mongodb.private_ip]
@@ -53,8 +60,128 @@ resource "aws_instance" "catalogue" {
 
 resource "aws_route53_record" "catalogue" {
   zone_id = "Z08406313PSKR2N4EROKD"
-  name    = "catalogued.e.platform.online"
+  name    = "catalogued.e-platform.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.catalogue.private_ip]
+}
+
+resource "aws_instance" "mysql" {
+  ami=data.aws_ami.centos_ami.image_id
+  instance_type = "var.instance_type"
+  tags = {
+    name="mysql"
+  }
+}
+resource "aws_route53_record" "mysql" {
+  zone_id = "var.zone_id"
+  name= "mysqld.e-platform.online"
+  type = "A"
+  ttl =30
+  records = [aws_instance.mysql.private_ip]
+}
+resource "aws_instance" "user" {
+  ami           = data.aws_ami.centos_ami.image_id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "user"
+  }
+}
+
+resource "aws_route53_record" "user" {
+  zone_id = "Z08406313PSKR2N4EROKD"
+  name    = "userd.e-platform.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
+resource "aws_instance" "cart" {
+  ami           = data.aws_ami.centos_ami.image_id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "cart"
+  }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = "Z08406313PSKR2N4EROKD"
+  name    = "cartd.e-platform.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
+resource "aws_instance" "redis" {
+  ami           = data.aws_ami.centos_ami.image_id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "redis"
+  }
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = "Z08406313PSKR2N4EROKD"
+  name    = "redisd.e-platform.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
+}
+resource "aws_instance" "rabbitmq" {
+  ami           = data.aws_ami.centos_ami.image_id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = "Z08406313PSKR2N4EROKD"
+  name    = "rabbitmqd.e-platform.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
+resource "aws_instance" "payment" {
+  ami           = data.aws_ami.centos_ami.image_id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "payment"
+  }
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = "Z08406313PSKR2N4EROKD"
+  name    = "paymentd.e-platform.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
+}
+resource "aws_instance" "shipping" {
+  ami           = data.aws_ami.centos_ami.image_id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "shipping"
+  }
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = "Z08406313PSKR2N4EROKD"
+  name    = "shippingd.e-platform.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
+resource "aws_instance" "dispatch" {
+  ami           = data.aws_ami.centos_ami.image_id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "dispatch"
+  }
+}
+
+resource "aws_route53_record" "dispatch" {
+  zone_id = "Z08406313PSKR2N4EROKD"
+  name    = "dispatchd.e-platform.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.dispatch.private_ip]
 }
