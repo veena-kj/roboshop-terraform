8e1ffea481df92960d4b7eb56ea8b88b0c89bd4c
DELL <kolloju.aishwarya@gmail.com>
2023-04-29 06:20:55 +0530

    < fetching ami Image ID output >

--- servers.tf
+++ servers.tf
@@ -8,3 +8,11 @@ resource "aws_instance" "frontend" {
 output "frontend" {
   value=aws_instance.frontend.public_ip
 }
+
+data "aws_ami" "centos_ami"{
+  owner = 973714476881
+  name_regex = "Centos-8-DevOps-Practice"
+  }
+output "ami_details" {
+  value = data.aws_ami.centos_ami.image_id
+}
