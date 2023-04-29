7e7276b8b5da264d0f903ca7f624b21dd89b75c0
DELL <kolloju.aishwarya@gmail.com>
2023-04-29 06:39:22 +0530

    < spel chek - public and private ip output >

--- servers.tf
+++ servers.tf
@@ -1,9 +1,17 @@
 resource "aws_instance" "frontend" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           = data.aws_ami.centos_ami.image_id
   instance_type = "t3.micro"
     tags = {
     Name = "frontend"
   }
+}
+
+data "aws_ami" "centos_ami"{
+  owners = ["973714476881"]
+  name_regex = "Centos-8-DevOps-Practice"
+}
+output "ami_details" {
+  value = data.aws_ami.centos_ami.image_id
 }
 output "frontend" {
   value=aws_instance.frontend.public_ip
@@ -12,10 +20,18 @@ output "frontendIP" {
   value=aws_instance.frontend.private_ip
 }
 
-data "aws_ami" "centos_ami"{
-  owners = ["973714476881"]
-  name_regex = "Centos-8-DevOps-Practice"
+resource "aws_instance" "mongodb" {
+  ami           = data.aws_ami.centos_ami.image_id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "mongodb"
   }
-output "ami_details" {
-  value = data.aws_ami.centos_ami.image_id
+}
+
+resource "aws_instance" "catalogue" {
+  ami           = data.aws_ami.centos_ami.image_id
+  instance_type = "t3.micro"
+  tags = {
+    Name = "catalogue"
+  }
 }
