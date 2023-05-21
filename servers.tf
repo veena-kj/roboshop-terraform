2f55e790cc88877498962a9e26fd112fd692be3b
DELL <kolloju.aishwarya@gmail.com>
2023-05-21 07:41:46 +0530

    < provosioner and script >

--- servers.tf
+++ servers.tf
@@ -4,13 +4,27 @@ resource "aws_instance" "instance" {
   instance_type = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.securitygrp.id]
   tags = {
-    Name = each.value["name"]
+    Name = each.value["name-${env}"]
+  }
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = self.private_ip
+    }
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/veena-kj/roboshop-shell.git",
+      "ccd roboshop-shell",
+      "sudo bash ${each.value["name"]}".sh
+    ]
   }
 }
 resource "aws_route53_record" "records" {
   for_each = var.components
   zone_id = "Z08406313PSKR2N4EROKD"
-  name    = "${each.value["name"]}-dev.e-platform.online"
+  name    = "${each.value["name"]}-${env}.e-platform.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance[each.value["name"]].private_ip]
